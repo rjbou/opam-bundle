3f08207d5873f2ceb139e6ec29f1628f4c6dd8d5
Raja Boujbel <rjbou@ocamlpro.com>
2023-03-30 13:13:08 +0200

    test

--- src/opamBundleMain.ml
+++ src/opamBundleMain.ml
@@ -305,7 +305,7 @@ let create_bundle ocamlv opamv repo debug output env test doc yes self_extract
       in
       let nv, opam =
         match OpamPinned.find_opam_file_in_source name src with
-        | Some f ->
+        | Some (f, _) ->
           OpamConsole.note
             "Will use package definition found in source for %s"
             (OpamPackage.Name.to_string name);
